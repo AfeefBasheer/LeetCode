f62e1c3808d19bfe34a14d4087faef2696b4ba43
Afeef Basheer <afeef@example.com>
2025-09-25 04:41:56 +0530

    day-17

---
+++ 104maximumDepthofBinaryTree.js
@@ -0,0 +1,21 @@
+////Problem - 138
+//Beats 100% in time complexity 
+//beats 100% in space complexity 
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxDepth = function(root) {
+    if (root === null) return 0; 
+    let leftDepth = maxDepth(root.left);
+    let rightDepth = maxDepth(root.right);
+    return 1 + Math.max(leftDepth, rightDepth);
+};
---
+++ 287findTheDuplciateNumber.js
@@ -0,0 +1,24 @@
+//Problem - 138
+//Beats 85% in time complexity
+//beats 75% in space complexity
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findDuplicate = function (nums) {
+  let slow = nums[0];
+  let fast = nums[0];
+
+  do {
+    slow = nums[slow];
+    fast = nums[nums[fast]];
+  } while (slow !== fast);
+
+  slow = nums[0];
+  while (slow !== fast) {
+    slow = nums[slow];
+    fast = nums[fast];
+  }
+
+  return slow;
+};
